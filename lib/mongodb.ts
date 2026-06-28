455adf8a1758bf4bdb14684b437afa1688144a2f
PREM RANJAN SHARMA <153382361+premranjansharma@users.noreply.github.com>
2026-06-28 14:54:55 +0530

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,13 +1,17 @@
-// lib/mongodb.js
+// lib/mongodb.ts
 import mongoose from 'mongoose';
 
 if (!process.env.MONGODB_URI) throw new Error('MONGODB_URI define nahi hai');
 
-const cached = global.mongoose ?? (global.mongoose = { conn: null, promise: null });
+type MongooseCache = { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null };
+
+const globalWithMongoose = global as typeof globalThis & { mongoose: MongooseCache };
+
+const cached: MongooseCache = globalWithMongoose.mongoose ?? (globalWithMongoose.mongoose = { conn: null, promise: null });
 
 const connectDB = async () => {
   if (cached.conn) return cached.conn;
-  if (!cached.promise) cached.promise = mongoose.connect(process.env.MONGODB_URI, { bufferCommands: false });
+  if (!cached.promise) cached.promise = mongoose.connect(process.env.MONGODB_URI!);
   cached.conn = await cached.promise;
   return cached.conn;
 };
